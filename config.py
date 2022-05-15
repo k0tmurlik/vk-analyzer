3837e4121ee6ddf2d6fda99eb1fbd5fec3614679
Vlad Surkov <tech@ltlp.ru>
2022-05-15 20:08:22 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+access_token = ""  # Токен vkhost.github.io
+handlers = {  # Обработчики
+    'removed_friends':  True,  # Автоматическая отписка от юзеров, которые удалили Вас из друзей
+    'relation_partner_removed': False  # Автоматическая смена СП
+}
+
+#True - Включено
+#False - Выключено
